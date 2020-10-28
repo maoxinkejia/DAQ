3507173576cc626ad2aa8c19c3a85acc95642492
zhangmaoxin <497122067@qq.com>
2020-10-28 17:43:17 +0800

    user列表bug

--- src/main/java/com/qcxk/controller/interceptor/RegistryConfiguration.java
+++ src/main/java/com/qcxk/controller/interceptor/RegistryConfiguration.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class RegistryConfiguration implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**");
+        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**");
     }
 }
